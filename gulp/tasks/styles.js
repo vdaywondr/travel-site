5da79ebaa479d8c6f2d8e368e1c975514e0b4e4b
Kishan <vdaywondr@gmail.com>
2017-12-06 01:04:09 -0500

    Learned to create Reusable Blocks and updated package.json

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp'),
 postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer')
+autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
