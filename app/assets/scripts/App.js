be150da29d57b12ea0f00cb46a53790e33b8e400
Kishan <vdaywondr@gmail.com>
2017-12-25 01:48:02 -0500

    Learned WebPack and automated it with Gulp.

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,6 @@
+var $ = require('jquery');
+var Person = require('./modules/Person');
+
+var john = new Person("John Doe", "blue");
+
+// $("h1").remove();
---
+++ app/assets/scripts/modules/Person.js
@@ -0,0 +1,6 @@
+function Person(f,c) {
+    this.name = f;
+    this.favColor = c;
+}
+
+module.exports = Person;
