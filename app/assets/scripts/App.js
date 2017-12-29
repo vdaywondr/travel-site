5328a601458da884ebd3972ed3bcd78ce5f05d3e
Kishan <vdaywondr@gmail.com>
2017-12-28 22:49:03 -0500

    Completed reusable reveal on scroll behavior.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "90%");
+new RevealOnScroll($(".testimonial"), "70%");
